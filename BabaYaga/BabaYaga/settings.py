b2030754a305c82a151f5da74ede4d3ec09e7eec
Rohit Sharma <rohit.sharma@coriolis.co.in>
2020-02-24 19:57:15 +0530

    Fix Login page html

--- BabaYaga/BabaYaga/settings.py
+++ BabaYaga/BabaYaga/settings.py
@@ -135,6 +135,7 @@ LOGIN_REDIRECT_URL = '/profile/home'
 
 LOGOUT_REDIRECT_URL = '/'
 
+MEDIA_URL = '/media/'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
