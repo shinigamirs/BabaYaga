e8c245238d95ec5ee88cd3bcea0aa53bfb05a09c
Rohit Sharma <rohit.sharma@coriolis.co.in>
2020-02-25 20:04:42 +0530

    Added pagination in booklist

--- BabaYaga/library/views.py
+++ BabaYaga/library/views.py
@@ -1,15 +1,29 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from library.models import *
+from django.views.generic import ListView
+from django.core.paginator import Paginator
+# # Create your views here.
 
-# Create your views here.
+# class PostListView(ListView):
+#     model = Book
+#     template_name = 'listing_books.html'
+#     context_object_name = 'books'
+#     ordering = ['title']
+#     paginate_by = 2
+
+
 def book_list(request, template_name="listing_books.html"):
     user = request.user
     name = user.first_name + " " + user.last_name
-    books = Book.objects.all()
+    books = Book.objects.all().order_by('title')
+    paginator = Paginator(books, 4)
+    page_number = request.GET.get('page')
+    books = paginator.get_page(page_number)
     context={
         'books' : books,
         'name' : name,
     }
+
     return render(request, template_name, context)
 
