82e25e4e07b47f8bb6c00a6df563550d23e22b3c
Rohit Sharma <rohit.sharma@coriolis.co.in>
2020-02-25 23:20:28 +0530

    Added Search functionality in book list

--- BabaYaga/library/views.py
+++ BabaYaga/library/views.py
@@ -4,13 +4,14 @@ from library.models import *
 from django.views.generic import ListView
 from django.core.paginator import Paginator
 # # Create your views here.
-
+from django.db.models import Q
 
 def book_list(request, template_name="listing_books.html"):
     user = request.user
     name = user.first_name + " " + user.last_name
     page_number = request.GET.get('page')
     search_query = request.GET.get('search', '')
+    print(search_query)
     books = Book.objects.all().order_by('title')
     if search_query:
         books = books.filter(Q(title__icontains=search_query) |
